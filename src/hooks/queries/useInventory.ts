6975f47bdc99891e28972b9d1b4f6f1d719853e0
fatihiazmi <fatihiazmi@gmail.com>
2025-06-11 02:25:14 +0800

    inventory details tab viewing and adding items feature added

--- src/hooks/queries/useInventory.ts
+++ src/hooks/queries/useInventory.ts
@@ -7,6 +7,16 @@ export const inventoryKeys = {
   sparePartsOptions: () => [...inventoryKeys.list(), "sparePartsOptions"],
   storeOptions: () => [...inventoryKeys.list(), "storeOptions"],
   rackNoOptions: () => [...inventoryKeys.list(), "rackNoOptions"],
+  workOrderOptions: () => [...inventoryKeys.list(), "workOrderOptions"],
+  adjustmentCategoryOptions: () => [...inventoryKeys.list(), "adjustmentCategoryOptions"],
+  adjustmentTypeOptions: () => [...inventoryKeys.list(), "adjustmentTypeOptions"],
+  employeeOptions: () => [...inventoryKeys.list(), "employeeOptions"],
+  receiveInventory: () => [...inventoryKeys.list(), "receiveInventory"],
+  issueInventory: () => [...inventoryKeys.list(), "issueInventory"],
+  returnInventory: () => [...inventoryKeys.list(), "returnInventory"],
+  adjustmentInventory: () => [...inventoryKeys.list(), "adjustmentInventory"],
+  transferInventory: () => [...inventoryKeys.list(), "transferInventory"],
+  transactionInventory: () => [...inventoryKeys.list(), "transactionInventory"],
 };
 
 
@@ -19,6 +29,105 @@ export const useInventoryList = (params?: {
   return useQuery({
     queryKey: [inventoryKeys.list(), params],
     queryFn: () => inventoryService.getInventoryList(params),
+  });
+}
+
+export const useInventoryDetail = (id: string) => {
+  return useQuery({
+    queryKey: inventoryKeys.detail(id),
+    queryFn: () => inventoryService.getInventoryById(Number(id)),
+  });
+}
+
+export const useReceiveInventory = () => {
+  return useQuery({
+    queryKey: inventoryKeys.receiveInventory(),
+    queryFn: () => inventoryService.getReceiveInventory(),
+  })
+}
+
+export const useAddReceiveInventory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: inventoryService.createReceiveInventory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: inventoryKeys.receiveInventory() });
+    },
+  });
+}
+
+export const useIssueInventory = () => {
+  return useQuery({
+    queryKey: inventoryKeys.issueInventory(),
+    queryFn: () => inventoryService.getIssueInventory(),
+  })
+}
+
+export const useAddIssueInventory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: inventoryService.createIssueInventory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: inventoryKeys.issueInventory() });
+    },
+  });
+}
+
+export const useReturnInventory = () => {
+  return useQuery({
+    queryKey: inventoryKeys.returnInventory(),
+    queryFn: () => inventoryService.getReturnInventory(),
+  })
+}
+
+export const useAddReturnInventory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: inventoryService.createReturnInventory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: inventoryKeys.returnInventory() });
+    },
+  });
+}
+
+export const useTransferInventory = () => {
+  return useQuery({
+    queryKey: inventoryKeys.transferInventory(),
+    queryFn: () => inventoryService.getTransferInventory(),
+  })
+}
+
+export const useAddTransferInventory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: inventoryService.createTransferInventory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: inventoryKeys.transferInventory() });
+    },
+  });
+}
+
+export const useTransactionInventory = () => {
+  return useQuery({
+    queryKey: inventoryKeys.transactionInventory(),
+    queryFn: () => inventoryService.getTransactionInventory(),
+  })
+}
+
+export const useAdjustmentInventory = () => {
+  return useQuery({
+    queryKey: inventoryKeys.adjustmentInventory(),
+    queryFn: () => inventoryService.getAdjustmentInventory(),
+  })
+}
+
+export const useAddAdjustmentInventory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: inventoryService.createAdjustmentInventory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: inventoryKeys.adjustmentInventory() });
+    },
   });
 }
 
@@ -50,5 +159,33 @@ export const useRackNoOptions = () => {
   return useQuery({
     queryKey: inventoryKeys.rackNoOptions(),
     queryFn: () => inventoryService.getRackNoOptions(),
+  });
+}
+
+export const useWorkOrderOptions = () => {
+  return useQuery({
+    queryKey: inventoryKeys.workOrderOptions(),
+    queryFn: () => inventoryService.getWorkOrderOptions(),
+  });
+}
+
+export const useAdjustmentCategoryOptions = () => {
+  return useQuery({
+    queryKey: inventoryKeys.adjustmentCategoryOptions(),
+    queryFn: () => inventoryService.getAdjustmentCategoryOptions(),
+  });
+}
+
+export const useAdjustmentTypeOptions = () => {
+  return useQuery({
+    queryKey: inventoryKeys.adjustmentTypeOptions(),
+    queryFn: () => inventoryService.getAdjustmentTypeOptions(),
+  });
+}
+
+export const useEmployeeOptions = () => {
+  return useQuery({
+    queryKey: inventoryKeys.employeeOptions(),
+    queryFn: () => inventoryService.getEmployeeOptions(),
   });
 }
